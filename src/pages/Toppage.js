15da88f891e1fabc650317e069e8c98c5d1a09b7
Hiro <hiromichi.s.617@gmail.com>
2021-07-18 18:08:23 +0900

    Selector追加

--- src/pages/Toppage.js
+++ src/pages/Toppage.js
@@ -1,11 +1,13 @@
-import Title from '../components/Title';
+import Selector from "../components/Selector";
+import Title from "../components/Title";
 
 const Toppage = () => {
-    return(
-        <div>
-            <Title />
-        </div>
-    )
-}
+	return (
+		<div>
+			<Title />
+			<Selector />
+		</div>
+	);
+};
 
 export default Toppage;
